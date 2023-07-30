cc0ed3755214ce27ffbe8edb9d2207a91cebc46c
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-07-30 20:32:46 +0000

    Exercícios Tipos de Variáveis

--- src/main/java/com/example/App1.java
+++ src/main/java/com/example/App1.java
@@ -5,9 +5,15 @@ public class App1 {
     public static void main(String[] args){
         int idadePessoa = 42;
         double pesoPessoa = 89.95;
+        char sexo = 'M';
+        boolean necessidadesEspeciais = true;
         System.out.println("Idade da Pessoa é " + idadePessoa);
         System.out.println("Peso da Pessoa é " + pesoPessoa);
+        System.out.println("O sexo da pessoa é " + sexo);
+        System.out.println(necessidadesEspeciais);
+
         //teste comentário
+        
     }
 
 }
