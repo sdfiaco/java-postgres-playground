2673495ed91b79edd283e322d4fa2d0d32821f06
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-07-30 21:16:28 +0000

    Aula de Arrays

---
+++ src/main/java/com/example/AppArrays.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.util.Arrays;
+
+public class AppArrays {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        double vetor [] = {10, 20, 30, 40, 50, 60, 70}; 
+        System.out.println(Arrays.toString(vetor));
+        System.out.println("Valor da primeira posição: " + vetor[0]);
+
+    }
+    
+}
