6633d609f1c9e2063701d5f8163db98c14f76526
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-07-30 19:43:56 +0000

    Primeiro Teste

---
+++ src/main/java/com/example/App1.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class App1 {
+
+    public static void main(String[] args){
+        System.out.println();
+        System.out.println();
+        System.out.println("Minha pirmeira aplicação Java");
+        
+    }
+
+}
