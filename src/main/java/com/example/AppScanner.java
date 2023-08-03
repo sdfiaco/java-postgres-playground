3654f7967eb503f07364e30664e740e2774cdba8
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-08-03 21:00:41 +0000

    Exercício Scanner

---
+++ src/main/java/com/example/AppScanner.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class AppScanner {
+
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe seu nome completo: ");
+        String nomeCliente = scanner.nextLine();
+        System.out.println();
+        System.out.println("Informe sua renda: ");
+        double renda = scanner.nextDouble();
+        System.out.println();
+        System.out.println("Informe seu ano de nascimento: ");
+        int anoNascimento = scanner.nextInt();
+        System.out.println();
+        System.out.println("Informe seu sexo: ");
+        String sexo = scanner.nextLine();
+
+        //if(renda < 0)
+         //renda = 0;
+       // if(sexo != 'M')
+            //if(sexo != 'F')
+            //System.out.println(" Digite apenas 'M' ou 'F'");
+        System.out.println("O nome do cliente é: " + nomeCliente);
+        System.out.println(" A renda do cliente é:" + renda);
+        System.out.println(" O ano de nascimento do cliente é:" + anoNascimento);
+        System.out.println(" O sexo do cliente é:" + sexo);
+    
+    
+
+        
+    }
+    
+}
