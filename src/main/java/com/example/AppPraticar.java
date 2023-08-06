43eaab73df342030d3d02608acd0e5451c42a85a
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-08-06 16:54:24 +0000

    06.08.23

--- src/main/java/com/example/AppPraticar.java
+++ src/main/java/com/example/AppPraticar.java
@@ -2,14 +2,11 @@ package com.example;
 
 public class AppPraticar {
     
-}
-public void Fatorial(){
-    
-    //System.out.println(fatorial);
-    
         public static void public static void main(String[] args) 
         {
-    
-            CalcFator ensaiar = new CalcFator();
-            System.out.println(ensaiar.fatorial);
+            Praticar ensaio = Praticar();
+            ensaio.primeiroNumeroIntervalo = 5;
+            ensaio.ultimoNumeroIntervalo = 105;
+            ensaio.fatorial= 10;
+
         }
--- src/main/java/com/example/Praticar.java
+++ src/main/java/com/example/Praticar.java
@@ -2,35 +2,36 @@ package com.example;
 
 import java.util.Scanner;
 
-public class Praticar {
+class Praticar {
     Scanner scanner = new Scanner(System.in);
     int primeiroNumeroIntervalo;
     int ultimoNumeroIntervalo;
     int fatorial;
     int parcialFatorial;
     int tamanhoFatorial;
+    int numeroParcial;
 
     public Intervalo()
     {
-        System.out.println("Digite o primeiro número do intervalo:");
-        primeiroNumeroIntervalo = scanner.nextInt();
-        System.out.println("Digite o último número do intervalo:");
-        ultimoNumeroIntervalo = scanner.nextInt();
+        //System.out.println("Digite o primeiro número do intervalo:");
+        //primeiroNumeroIntervalo = scanner.nextInt();
+        //System.out.println("Digite o último número do intervalo:");
+        //ultimoNumeroIntervalo = scanner.nextInt();
         
         for(int i = primeiroNumeroIntervalo; i<=ultimoNumeroIntervalo ; i++)
         {
-            System.out.println(i);
+            numeroParcial = i;
+            System.out.println(numeroParcial);
         }
 
     }
 
-    public CalcFator() 
+    public CalcFator()
     {
-        
-        System.out.println("Digite o número para cálculo do fatorial: ");
-        fatorial = scanner.nextInt();
+        //System.out.println("Digite o número para cálculo do fatorial: ");
+        //fatorial = scanner.nextInt();
         tamanhoFatorial = fatorial; 
-        parcialFatorial = 1;
+        
         for(int i = tamanhoFatorial; i <fatorial; i--)
         {
         parcialFatorial = parcialFatorial * i;
