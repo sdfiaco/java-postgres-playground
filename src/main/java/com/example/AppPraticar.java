42c60a5111241275e2e33d2955966c4bc8128b02
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-08-06 18:10:47 +0000

    06.08.23

--- src/main/java/com/example/AppPraticar.java
+++ src/main/java/com/example/AppPraticar.java
@@ -1,12 +1,17 @@
 package com.example;
 
-public class AppPraticar {
-    
-        public static void public static void main(String[] args) 
+public class AppPraticar 
+{
+        public static void main(String[] args) 
         {
-            Praticar ensaio = Praticar();
-            ensaio.primeiroNumeroIntervalo = 5;
-            ensaio.ultimoNumeroIntervalo = 105;
-            ensaio.fatorial= 10;
+            int fatorial = 10;
+            int parcialFatorial = 1;
+            for(int i = fatorial; i>0; i--)
+            {
+            fatorial = i * parcialFatorial;
+            parcialFatorial = fatorial; 
+            }
+            System.out.println(fatorial);
 
         }
+}
--- src/main/java/com/example/Praticar.java
+++ src/main/java/com/example/Praticar.java
@@ -1,42 +1,19 @@
 package com.example;
 
-import java.util.Scanner;
 
 class Praticar {
-    Scanner scanner = new Scanner(System.in);
-    int primeiroNumeroIntervalo;
-    int ultimoNumeroIntervalo;
     int fatorial;
-    int parcialFatorial;
+    int parcialFatorial = 1;
     int tamanhoFatorial;
-    int numeroParcial;
 
-    public Intervalo()
+    public Praticar()
     {
-        //System.out.println("Digite o primeiro número do intervalo:");
-        //primeiroNumeroIntervalo = scanner.nextInt();
-        //System.out.println("Digite o último número do intervalo:");
-        //ultimoNumeroIntervalo = scanner.nextInt();
-        
-        for(int i = primeiroNumeroIntervalo; i<=ultimoNumeroIntervalo ; i++)
+        for(int i = tamanhoFatorial; i>0; i--)
         {
-            numeroParcial = i;
-            System.out.println(numeroParcial);
+        fatorial = i * parcialFatorial;
+        parcialFatorial = fatorial; 
         }
-
-    }
-
-    public CalcFator()
-    {
-        //System.out.println("Digite o número para cálculo do fatorial: ");
-        //fatorial = scanner.nextInt();
-        tamanhoFatorial = fatorial; 
-        
-        for(int i = tamanhoFatorial; i <fatorial; i--)
-        {
-        parcialFatorial = parcialFatorial * i;
-        }
-        fatorial = parcialFatorial;
+        //fatorial = parcialFatorial;
 
     }
   
