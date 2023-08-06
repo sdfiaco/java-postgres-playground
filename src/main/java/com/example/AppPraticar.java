782b4d4e36eb083424d6ddf2bea32fd7c8be3b0a
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-08-06 16:30:31 +0000

    06.08.23

---
+++ src/main/java/com/example/AppPraticar.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class AppPraticar {
+    
+}
+public void Fatorial(){
+    
+    //System.out.println(fatorial);
+    
+        public static void public static void main(String[] args) 
+        {
+    
+            CalcFator ensaiar = new CalcFator();
+            System.out.println(ensaiar.fatorial);
+        }
--- src/main/java/com/example/Praticar.java
+++ src/main/java/com/example/Praticar.java
@@ -2,51 +2,44 @@ package com.example;
 
 import java.util.Scanner;
 
-class Praticar {
-    //int somaAcumulada;
-    //double achaMultiplo3;
-    //long fatorial;
-    //char multiplo3;
+public class Praticar {
+    Scanner scanner = new Scanner(System.in);
+    int primeiroNumeroIntervalo;
+    int ultimoNumeroIntervalo;
+    int fatorial;
+    int parcialFatorial;
+    int tamanhoFatorial;
 
-    public Intervalo(){
-        int primeiroNumeroIntervalo;
-        int ultimoNumeroIntervalo;
+    public Intervalo()
+    {
         System.out.println("Digite o primeiro número do intervalo:");
         primeiroNumeroIntervalo = scanner.nextInt();
         System.out.println("Digite o último número do intervalo:");
         ultimoNumeroIntervalo = scanner.nextInt();
         
-        for(int i = primeiroNumeroIntervalo; i<=ultimoNumeroIntervalo ; i++){
+        for(int i = primeiroNumeroIntervalo; i<=ultimoNumeroIntervalo ; i++)
+        {
             System.out.println(i);
         }
 
     }
 
-    pubic CalcFator() {
-        int fatorial;
-        int parcialFatorial;
-        int tamanhoFatorial;
+    public CalcFator() 
+    {
         
-        public Fatorial(){
         System.out.println("Digite o número para cálculo do fatorial: ");
         fatorial = scanner.nextInt();
         tamanhoFatorial = fatorial; 
         parcialFatorial = 1;
-        for(int i = tamanhoFatorial; i <fatorial; i--){
-            parcialFatorial = parcialFatorial * i;
+        for(int i = tamanhoFatorial; i <fatorial; i--)
+        {
+        parcialFatorial = parcialFatorial * i;
         }
         fatorial = parcialFatorial;
-        System.out.println(fatorial);
-        }
-    }
 
-    public static void public static void main(String[] args) {
-        
     }
+  
 }
-
-
-
 
     
 
