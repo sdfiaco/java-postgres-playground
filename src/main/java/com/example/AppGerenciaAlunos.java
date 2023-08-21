ab07be46cf208cdc448cab286124cf9e501c2799
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-08-21 03:12:43 +0000

    21.08.23

---
+++ src/main/java/com/example/AppGerenciaAlunos.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AppGerenciaAlunos {
+
+public static void main(String[] args) 
+
+{
+
+    Scanner scanner = new Scanner(System.in);
+    int numeroMaximoAlunosTurma = 30;
+    String insereAluno;
+    String alunosTurma[][] = new String [6][6]; 
+    alunosTurma[0][0] = "Nome";
+    alunosTurma[0][1] = "Matemática";
+    alunosTurma[0][2] = "Português";
+    alunosTurma[0][3] = "Ciências";
+    alunosTurma[0][4] = "Geografia";
+    alunosTurma[0][5] = "História";
+    int contAluno = 1;
+
+    System.out.println("Deseja Cadastrar Aluno? ");
+    insereAluno = scanner.nextLine();
+
+    if (insereAluno.equals("SIM"))
+    {
+        while(insereAluno.equals("SIM") && contAluno<6)
+        {
+            
+            //scanner.nextLine();
+            System.out.println("Digite o nome do aluno a ser inserido: ");
+            alunosTurma[contAluno][0] = scanner.nextLine();
+            contAluno++; 
+            //System.out.println(alunosTurma[contAluno][0]); 
+            for (int i=0; i<6; i++)
+            {
+                System.out.println("Digite a nota da disciplina");
+                alunosTurma[contAluno][i] = scanner.nextLine();
+            }
+            //contAluno++; 
+            System.out.println("Deseja inserir outro aluno? ");
+            insereAluno = scanner.nextLine();
+        }    
+    }
+
+    for(int j=0; j<contAluno; j++)
+    System.out.println(Arrays.toString(alunosTurma[j]);
+
+}
+
+} 
+
+
+
+
+
