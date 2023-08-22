4b8e87e76e0ef6f3691af155a7dfd64d084b54b1
Silverio Del Fiaco <silverio.fiaco@gmail.com>
2023-08-22 18:08:17 +0000

    22.08.23

---
+++ src/main/java/com/example/decoralista.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class decoralista 
+{
+
+    ArrayList<String> lista = new ArrayList<String>();
+    int QtdNomes;
+    String imprimeLista;
+    String insereNome; 
+
+
+    public String getInsereNome()
+    {  
+        String insereOutro = "SIM";
+        int QtdNomes=1;
+        Scanner scanner = new Scanner(System.in);
+        while(insereOutro.equals("SIM"))
+        {
+            System.out.println("Digite o " + QtdNomes + "º nome: ");
+            insereNome = scanner.nextLine();
+            lista.add(insereNome);
+            QtdNomes++;
+            System.out.println("Deseja adicionar outro nome: sim ou não: ");
+            insereOutro = scanner.nextLine();   
+            insereOutro = insereOutro.toUpperCase(); 
+        }
+        System.out.println("Foram inseridos " + QtdNomes + "nomes.");
+        return ("Inserção Concluída.");
+    }
+
+
+    public String getImprimeLista() 
+    {
+        Collections.sort(lista);
+        for(var nome : lista)
+        System.out.println(nome);
+        return ("Está é a lista completa!");
+    }
+    
+    
+}
